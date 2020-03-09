5d4ee84d991d5f4429a640b4b5c7182f138fa3ef
balajipalakkattu <balajipalakkattu@gmail.com>
2020-03-08 20:28:39 -0500

    added load balancer- spring cloud ribbon

--- src/main/java/com/learnings/employee/controller/ConsumerControllerTestClient.java
+++ src/main/java/com/learnings/employee/controller/ConsumerControllerTestClient.java
@@ -6,31 +6,36 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.client.RestTemplate;
 
 
 public class ConsumerControllerTestClient {
 
 	@Autowired
-	private DiscoveryClient discoveryClient;
+	// private DiscoveryClient discoveryClient;
+	private LoadBalancerClient discoveryClient;
 
 	@Value("${service.id}")
 	private String serviceId;
 
 	public void getEmployee() {
 		RestTemplate restTemplate = new RestTemplate();
-System.out.println("BG: service id-->" + serviceId);
+		System.out.println("BG: service id-->" + serviceId);
 
-		List<ServiceInstance> clientInfo = discoveryClient.getInstances(serviceId);
-		ServiceInstance serviceInstance = clientInfo == null ? null : clientInfo.get(0);
-
-		String baseUrl = serviceInstance == null ? "http://localhost:8080/employee" : serviceInstance.getUri().toString() + "/employee";
-
+		// List<ServiceInstance> clientInfo = discoveryClient.getInstances(serviceId);
+		// ServiceInstance serviceInstance = clientInfo == null ? null :
+		// clientInfo.get(0);
+		ServiceInstance serviceInstance = discoveryClient.choose(serviceId);
+		String baseUrl = serviceInstance == null ? "http://localhost:8080/employee"
+				: serviceInstance.getUri().toString() + "/employee";
+System.out.println(serviceInstance.getUri().toString());
 		ResponseEntity<String> response = restTemplate.exchange(baseUrl, HttpMethod.GET, getHttpEntity(), String.class);
 
 		System.out.println(response.getBody());
